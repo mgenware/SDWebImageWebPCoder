a79650a8e7107a3bbbb4b4564c0231523ffa762e
Mgen <m@mgenware.com>
2025-03-09 19:49:04 +0800

    Pin libwebp-Xcode

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.17.0"),
-        .package(url: "https://github.com/SDWebImage/libwebp-Xcode.git", from: "1.1.0")
+        .package(url: "https://github.com/SDWebImage/libwebp-Xcode.git", exact: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
